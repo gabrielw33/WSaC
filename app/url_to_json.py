aa9bfac8f5212ff3532c24f042805fba6942b470
WlodekLipski <238961@student.pwr.edu.pl>
2019-08-22 12:58:13 +0200

    Code refactoring. Added 'target' folder, which will contain files to send. Added dictionary values for 'json' and 'xml' files.

--- app/url_to_json.py
+++ app/url_to_json.py
@@ -3,9 +3,9 @@ import sys
 import json
 import argparse
 import requests
-from datetime import datetime
 
-def Url_to_json(url,reg):
+
+def Url_to_json(url, reg):
     responce = requests.get(url)
     dictionary = {}
     match = re.finditer(reg, responce.text)
@@ -13,7 +13,7 @@ def Url_to_json(url,reg):
         key = match.group()
         dictionary[key] = -2
     json_from_dict = json.dumps(dictionary, indent=4)
-    json_file = open(datetime.now().strftime('%H:%M:%S')+"_dict.json", "w")
+    json_file = open("dict.json", "w+")
     json_file.write(json_from_dict)
     json_file.close()
     print("ok")
