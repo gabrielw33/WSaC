75eb445dd59895448f03d7fc3bf264eecbad3cf7
gabrielw333 <gabriel.w33@protonmail.com>
2019-08-27 09:59:43 +0200

    travis-codestyle3

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ ALLOWED_EXTENSIONS = {'json', 'xml'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1) \
-        [1].lower() in ALLOWED_EXTENSIONS
+     [1].lower() in ALLOWED_EXTENSIONS
 
 
 app = Flask(__name__)
